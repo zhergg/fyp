99d8f4ef87f6fea851df9e1915824b84e8908bb3
zhergg <guongzer@naver.com>
2024-11-22 09:56:32 +0800

    Update fyp2point3.py

--- fyp2point3.py
+++ fyp2point3.py
@@ -8,11 +8,11 @@ import string
 import time
 
 # Load the Logistic Regression model and vectorizer
-log_model = joblib.load(logistic_regression_model.pkl)
-vectorizer = joblib.load(vectoriser.pkl)
+log_model = joblib.load('logistic_regression_model.pkl')
+vectorizer = joblib.load('vectoriser.pkl')
 
 # Initialize the SQLite database
-conn = sqlite3.connect(users.db, check_same_thread=False)
+conn = sqlite3.connect('users.db', check_same_thread=False)
 c = conn.cursor()
 
 # Create user and comment tables if they don't exist
